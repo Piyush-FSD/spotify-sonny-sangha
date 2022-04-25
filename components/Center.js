3e3972ccfb7d16cb4512db98d1a0f526464765b4
Piyush Patel <piyushpatel.cloud@yahoo.com>
2022-04-25 19:28:30 -0400

    build songs component

--- components/Center.js
+++ components/Center.js
@@ -5,6 +5,10 @@ import { useEffect } from "react";
 import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import SpotifyWebApi from "spotify-web-api-node";
+import { spotifyAPI } from '../lib/spotify'
+import { useSpotify } from "../hooks/useSpotify";
+import { Songs } from "./Songs";
 
 const colors = [
     "from-indigo-500",
@@ -18,6 +22,7 @@ const colors = [
 
 export const Center = () => {
     const { data: session } = useSession();
+    const spotifyAPI = useSpotify()
     const [color, setColor] = useState(null)
     const playlistId = useRecoilValue(playlistIdState)
     const [playlist, setPlaylist] = useRecoilState(playlistState)
@@ -26,10 +31,17 @@ export const Center = () => {
         setColor(shuffle(colors).pop())
     }, [playlistId])
 
+    useEffect(() => {
+        spotifyAPI.getPlaylist(playlistId).then((data) => {
+            setPlaylist(data.body)
+        })
+            .catch((err) => console.log("Something went wrong:", err))
+    }, [spotifyAPI, playlistId])
+
     return (
         <div className="flex-grow">
             <header className="absolute top-5 right-8">
-                <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
                     <img
                         className="rounded-full w-10 h-10"
                         src={session?.user.image}
@@ -38,10 +50,19 @@ export const Center = () => {
                     <ChevronDownIcon className="h-5 w-5" />
                 </div>
             </header>
-            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8 w-full`}>
-                {/* <img src="" /> */}
-                <h2>hello</h2>
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}>
+                <img
+                    className="h-44 w-44 shadow-2xl"
+                    src={playlist?.images?.[0].url}
+                    alt="" />
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+                </div>
             </section>
+            <div>
+                <Songs />
+            </div>
         </div>
     )
 }
