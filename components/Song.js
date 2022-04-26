390a40c1f3df9312387f1232bbf52d623bc5fe6b
Piyush Patel <piyushpatel.cloud@yahoo.com>
2022-04-25 23:34:15 -0400

    building song component

---
+++ components/Song.js
@@ -0,0 +1,6 @@
+export const Song = ({ order, track }) => {
+    return (
+        <div>
+
+        </div>)
+}
--- components/Songs.js
+++ components/Songs.js
@@ -1,3 +1,15 @@
-export const Songs = () => {
+import { useRecoilValue } from "recoil"
+import { playlistState } from "../atoms/playlistAtom"
+import { Song } from './Song'
 
+export const Songs = () => {
+    const playlist = useRecoilValue(playlistState)
+
+    return (
+        <div className="px-8 flex flex-col space-y-1 pb-28 text-white">
+            {playlist?.tracks?.items.map((item, idx) => (
+                <Song key={item.track.id} track={item} order={idx} />
+            ))}
+        </div>
+    )
 }
